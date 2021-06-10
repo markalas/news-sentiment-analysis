cf3030330dac7bfaf36e18604d28344fdd1d5934
Marklexter Alas <marklexteralas@gmail.com>
2021-06-10 16:15:57 -0400

    Created new columns total_reacts, post_category, and status_category

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import os
 import pandas as pd
 import numpy as np
+import nltk as nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+
 
 # Setup working directory and relative filepaths
 current_dir = os.curdir
@@ -26,9 +30,47 @@ cbs_data_df = pd.DataFrame(cbs_data_csv.copy())
 cnn_data_df = pd.DataFrame(cnn_data_csv.copy())
 
 # Remove columns from Dataframe
-col_ls = ['id', 'page_id', 'name', 'message', 'description', 'caption', 'link', 'picture']
-abc_data_df.drop(columns = col_ls, inplace = True) 
-bbc_data_df.drop(columns = col_ls, inplace = True) 
+col_ls = ['id', 'page_id', 'message', 'description', 'caption', 'link', 'picture']
+abc_data_df.drop(columns = col_ls, inplace = True)
+bbc_data_df.drop(columns = col_ls, inplace = True)
 cbs_data_df.drop(columns = col_ls, inplace = True)
 cnn_data_df.drop(columns = col_ls, inplace = True)
+
+# Determine how many post_types and status_types and factorize types
+post_types_unique = pd.unique(abc_data_df['post_type'])
+post_types_factorize = pd.factorize(post_types_unique, sort=True)[0]
+status_types_unique = pd.unique(abc_data_df['status_type'])
+status_types_unique = [i for i in status_types_unique if str(i) != 'nan']
+print(status_types_unique)
+status_types_factorize = pd.factorize(status_types_unique, sort=True)[0]
+
+# Create dictionary for post_types and status_types
+post_types_dict = dict(zip(post_types_unique, post_types_factorize))
+status_types_dict = dict(zip(status_types_unique, status_types_factorize))
+
+# Define function to assign kv pairs to post_types and status_types
+def post_cat(x):
+    for type, category in post_types_dict.items():
+        if x in type:
+            return category
+
+def status_cat(x):
+    for type, category in status_types_dict.items():
+        try:
+            if x in type:
+                return category
+        except TypeError:
+            return -1
+
+# Apply function to DataFrame
+abc_data_df['post_category'] = abc_data_df['post_type'].apply(post_cat)
+abc_data_df['status_category'] = abc_data_df['status_type'].apply(status_cat)
+
+# Calculate total reacts
+react_col = ['likes_count', 'comments_count', 'shares_count', 'love_count',
+       'wow_count', 'haha_count', 'sad_count', 'thankful_count', 'angry_count',]
+       
+abc_data_df['total_reacts'] = abc_data_df[react_col].sum(axis=1)
+
+# ToDo: Create Class to clean all dataframes at once
 
