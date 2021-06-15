972b391f22fc9bbf98e7d90f87d573c0c265ae43
Marklexter Alas <marklexteralas@gmail.com>
2021-06-15 19:48:34 -0400

    Created class for bulk cleaning df

--- main.py
+++ main.py
@@ -96,20 +96,89 @@ def analyze_sentiment(headline):
 
 # Create new column on abc_data_df called 'Sentiment' and store sentiment values using analyze_sentiment method
 abc_data_df[['name_sentiment', 'name_subjectivity']] = np.array([analyze_sentiment(str(headline)) for headline in abc_data_df['name']])
-print(abc_data_df.tail(15))
 
 # Export dataframe as csv to data_clean
 # abc_data_df.to_csv(os.path.join(output_dir, 'abc_data_clean.csv'), index=False)
 
-# Barchart of post_type count
+class CleanData:
+    def __init__(self, df): # initialize CleanData
+        self.df = df
+
+    def show_df(self): # Show dateframe object when passed to CleanData
+        return self.df
+    
+    def news_outlet(self, outlet=''):
+        self.df['news_outlet'] = outlet
+        pass
+
+    def calculate_reacts(self): # Calculate reacts for self.df
+
+        self.react_col = ['likes_count', 'comments_count', 'shares_count', 'love_count', # store columns of interest 
+            'wow_count', 'haha_count', 'sad_count', 'thankful_count', 'angry_count',]
+
+        self.df['total_reacts'] = self.df[self.react_col].sum(axis=1) # sum self.react_col by row
+
+        return self.df
+
+    def clean_datetime(self): # Clean datetime of self.df
+
+        def round_hour(t):
+            t_start_hr = t.replace(minute=0, second=0, microsecond=0) # round to down to nearest hour
+            t = t_start_hr
+            return t
+
+        self.df['posted_at'] = pd.to_datetime(self.df['posted_at']) # change dtype to datetime
+        self.df['posted_at'] = self.df['posted_at'].apply(round_hour) # apply round_hour
+        
+        self.df['date_posted'] = [date.date() for date in self.df['posted_at']] # store date() from datetime object
+        self.df['time_posted'] = [time.time() for time in self.df['posted_at']] # store time() from datetime object
+        self.df.drop(columns=['posted_at'], inplace=True)
+
+        return self.df
+    
+    def analyze_df(self):
+
+        def analyze_sentiment(headline):
+            analysis = TextBlob(str(headline))
+            return analysis.sentiment # returns sentiment, subjectivity
+
+        self.df[['name_sentiment', 'name_subjectivity']] = np.array([analyze_sentiment(str(headline)) for headline in self.df['name']]) # store sentiment, subjectivity in new columns
+
+        return self.df
+
+    def fill_na(self):
+        self.df['status_type'] = self.df['status_type'].fillna(self.df.mode().iloc[0][2]) # self.df.mode().iloc[0] returns a list of the most frequent values for each column in self.df iloc[0][2] accesses status_type
+        return self.df
+
+# BBC Data
+bbc_data_clean = CleanData(bbc_data_df)
+bbc_data_clean.news_outlet(outlet='BBC')
+bbc_data_clean.fill_na()
+bbc_data_clean.calculate_reacts()
+bbc_data_clean.clean_datetime()
+bbc_data_clean.analyze_df()
+bbc_data_clean = bbc_data_clean.show_df()
+print(bbc_data_clean)
+
+# Plot BBC Data
 chart1 = plt.figure(1)
+objects = pd.unique(bbc_data_clean['post_type'])
+y_pos = np.arange(len(objects))
+performance = bbc_data_clean['post_type'].value_counts()
+plt.bar(y_pos, performance, align='center', alpha=0.5)
+plt.xticks(y_pos, objects)
+
+
+# Barchart of post_type count
+chart1 = plt.figure(2)
 objects = post_types_unique
 y_pos = np.arange(len(objects))
-performance = abc_data_df['post_category'].value_counts()
+performance = abc_data_df['post_type'].value_counts()
 plt.bar(y_pos, performance, align='center', alpha=0.5)
 plt.xticks(y_pos, objects)
 plt.ylabel('Count')
 plt.title('Facebook ABC News Post: Post Type Comparison')
+plt.show()
 
 # Barchart of status_type count
 chart2 = plt.figure(2)
@@ -147,7 +216,12 @@ x_sentimentArray = abc_data_sample['name_sentiment'].to_numpy()
 y_subjectivityArray = abc_data_sample['name_subjectivity'].to_numpy()
 plt.scatter(x_sentimentArray, y_subjectivityArray, s=2)
 
-# plt.show()
+# Show sentiment density using seaborn
+chart = plt.figure(6)
+sentiment_density = sns.kdeplot(data=abc_data_sample, y='name_subjectivity', x='name_sentiment', fill=True)
+sentiment_density.set(xlabel='Polarity', ylabel='Subjectivity', title='Sentiment Density Map of ABC News Headlines')
+
+plt.show()
 
 # ToDo: 
     # Create Class to clean all dataframes at once
